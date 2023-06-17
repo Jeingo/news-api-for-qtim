6892e4ee01b0817e0335ca2459910bfeda65edea
Renat <jeingo20@gmail.com>
2023-06-17 15:52:23 +0300

    add: global validator pipe, exception filter

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ConfigType } from './configuration';
+import { ValidationPipe } from '@nestjs/common';
+import cookieParser from 'cookie-parser';
+import { HttpExceptionFilter } from './utils/exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService<ConfigType>);
+
+  app.enableCors();
+  app.use(cookieParser());
+  app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(configService.get('port'));
 }
 bootstrap();
